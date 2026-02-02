78247e6f853be41a0766898d2df03cdcb9b5f6a8
kudeee <codysu08@gmail.com>
2026-02-02 13:52:13 +0800

    small adjustments

--- js/payment-content.js
+++ js/payment-content.js
@@ -4,6 +4,6 @@ const type = params.get("type");
 if (type === "upgrade") {
   document.querySelector(".header-name").innerHTML = "Upgrade Membership";
   document.querySelector(".payment-btn").innerHTML = "UPGRADE";
-
+  document.querySelector(".page-title").innerHTML = "Upgrade Membership";
   document.getElementById("mem-plan").style.display = "none";
 }
